b96efc6afe3b77d8d58be1bd03f8130cfb01a665
Ali Sinan Çoban <alisinancoban@gmail.com>
2017-01-18 13:58:02 +0300

    Hareket Eklendi Duzeltilecek

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float moveSpeed = 5;
+
+    private Vector3 moveVector = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + moveVector);
+    }
+
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+
+        moveVector = (inputH * transform.right * moveSpeed) + (inputV * transform.forward * moveSpeed);
+    }
+}
